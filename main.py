703301719525263a609c0b4420c3b77a3915a6fa
Dmitry Kalalb <gdydhd0134h@gmail.com>
2020-10-08 19:28:00 +0200

    Создал логику приложения

---
+++ main.py
@@ -0,0 +1,94 @@
+import sys
+import platform
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect,
+                          QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence,
+                         QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import *
+
+# ? ==> SPLASH SCREEN
+from ui_splash_screen import Ui_SplashScreen
+
+# ? ==> MAIN WINDOW
+from ui_main import Ui_MainWindow
+
+# ? ==> Глобальные переменные
+counter = 0
+
+
+# * Приложение
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # MAIN WINDOW LABEL
+
+
+# ? Экран загруки (SPLASH SCREEN)
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_SplashScreen()
+        self.ui.setupUi(self)
+
+        # ? UI ==> INTERFACE CODES
+        # ? REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # ? DROP SHADOW EFFECT
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 60))
+        self.ui.dropShadowFrame.setGraphicsEffect(self.shadow)
+
+        # ? QTIMER ==> START
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        # ! Настройка таймера на 35 миллисекунд
+        self.timer.start(35)
+
+        # / Смена текста
+        # / Первый текст
+        self.ui.label_description.setText("<strong>Добро пожаловать</strong> в салон красоты")
+
+        # / Текст который придет на смену первому
+        QtCore.QTimer.singleShot(1500, lambda: self.ui.label_description.setText("<strong>Загрузка</strong> Базы данных"))
+        QtCore.QTimer.singleShot(3000, lambda: self.ui.label_description.setText("<strong>Загрузка</strong> Интерфейса"))
+
+        # ? SHOW ==> MAIN WINDOW
+        self.show()
+        # ? ==> END ##
+
+    # ? ==> APP FUNCTIONS
+    def progress(self):
+        global counter
+
+        # * SET VALUE TO PROGRESS BAR
+        self.ui.progressBar.setValue(counter)
+
+        # / Закрыть окно с загрузкой и открыть главное окно
+        if counter > 100:
+            # ? STOP TIMER
+            self.timer.stop()
+
+            # ? Показать MainWindow
+            self.main = MainWindow()
+            self.main.show()
+
+            # ! Закрыть окно загрузки
+            self.close()
+
+        # * Увеличить процент
+        counter += 1
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SplashScreen()
+    sys.exit(app.exec_())
