b4a2ed100b99623d34ef7774a45a668654190c30
Dmitry Kalalb <gdydhd0134h@gmail.com>
2020-11-20 22:03:39 +0200

    Добавил диалоговое окно для вывода ошибки

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,4 @@
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMessageBox
 
 
@@ -5,6 +6,7 @@ from PyQt5.QtWidgets import QMessageBox
 def show_popup(title, message, function):
     msg = QMessageBox()
     msg.setWindowTitle(title)
+    msg.setWindowIcon(QIcon('images/beauty_logo.ico'))
     msg.setText(title)
     msg.setIcon(QMessageBox.Question)
     msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
@@ -12,4 +14,19 @@ def show_popup(title, message, function):
     msg.setInformativeText(message)
 
     msg.buttonClicked.connect(function)
+    msg.exec_()
+
+
+# ! Диалоговое окно для ошибок
+def show_error_popup(error_msg):
+    msg = QMessageBox()
+    msg.setWindowTitle('Ошибка')
+    msg.setWindowIcon(QIcon('images/beauty_logo.ico'))
+    msg.setMinimumWidth(300)
+    msg.setIcon(QMessageBox.Critical)
+    msg.setStandardButtons(QMessageBox.Ok)
+    msg.setDefaultButton(QMessageBox.Ok)
+    msg.setText('По каким-то причинам произошла ошибка              ')
+    msg.setDetailedText(str(error_msg))
+
     msg.exec_()
