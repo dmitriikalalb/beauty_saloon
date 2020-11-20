aa85ad40382a2c0981edcc5480d8e01a0db5492f
Dmitry Kalalb <gdydhd0134h@gmail.com>
2020-11-20 18:37:52 +0200

    Хранение глобальных стилей

---
+++ utils/styleSheet.py
@@ -0,0 +1,8 @@
+TEXT_EDIT = 'width: 100%;' \
+            'box-sizing: border-box;' \
+            'border: 2px solid #ccc;' \
+            'border-radius: 4px;' \
+            'background-color: white;' \
+            'background-position: 10px 10px;' \
+            'background-repeat: no-repeat;' \
+            'padding: 12px 20px 12px 40px;'
